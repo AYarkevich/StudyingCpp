b28c26c0f273e44a63c73ee1830e6c67e89215ff
artsiom.yarkevich <evrosrv@gmail.com>
2022-08-21 01:37:53 +0300

    Added libs and removed fetching data

--- tests/LevelOneTests.cpp
+++ tests/LevelOneTests.cpp
@@ -7,9 +7,3 @@ const auto expected = "hello world!";
 const auto actual = greet();
 ASSERT_EQ(expected, actual);
 }
-
-int main(int argc, char** argv)
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
